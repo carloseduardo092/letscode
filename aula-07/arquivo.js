17e5d76dbdec449a43caa4da33a64c06048ad2de
Kadu <kadujunior567@gmail.com>
2024-12-26 13:59:07 -0400

    5.2

--- aula-07/arquivo.js
+++ aula-07/arquivo.js
@@ -1,20 +1,39 @@
 /*eslint-disable */
-const path = require('path')
-const fs = require('node:fs')
+const path = require("path")
+const fs = require("node:fs")
+const dotenv = require("dotenv")
+dotenv.config()
+console.log(process.env.NODE_ENV)
 
-const filepath = path.join(process.cwd(), 'aula-07', 'text.txt')
+const filepath = path.join(process.cwd(), "aula-07", "text.txt")
+const fileOutpath = path.join(process.cwd(), "aula-07","texto-com-linhas.txt")
+console.log(filepath)
+
+console.time("leitura do arquivo")
+console.time("manipular arquivos")
 
 fs.readFile(filepath, {}, (erro, dados) => {
-    if(erro) {
-        console.log(`Erro na leitura do caminho ${filepath}`)
+  if (erro) {
+    console.error(`Erro na leitura do caminho ${filepath}`)
+    return
+  }
+
+  console.timeEnd("leitura do arquivo")
+
+
+  const texto = dados.toString();
+  const linhas = texto.split("\n")
+
+  const linhasAjustadas = linhas.map(
+    (linha, index, arrayDelinhas) => `${index + 1} - ${linha}`
+  )
+  
+  fs.writeFile(fileOutpath, linhasAjustadas.join("\n"), {}, (erro) => {
+    if (erro) {
+        console.log(`Erro na escrita do caminho ${fileOutpath}`)
         return
-    }
-    const texto = dados.toString()
-    const linhas = texto.split('\n')
-
-    linhas.forEach((linha, index, arrayDelinhas) => 
-        console.log(`${index + 1} - ${linha}`))
-
-    // console.log(dados.toString())
-}
-)
+      }
+      console.log(`Arquivo salvo no bucket ${process.env.S3_BUCKET}`)
+      console.timeEnd("manipular arquivos")
+  })
+})
